88f9bf7ff6607e08e082be418b54066abbd0dacc
EkateK1 <144279877+EkateK1@users.noreply.github.com>
2023-12-02 01:57:39 +0300

    fix  collisions

--- MyHashMap/src/MyHashMap.java
+++ MyHashMap/src/MyHashMap.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class MyHashMap<K, V>
 {
 
@@ -20,12 +22,21 @@ public class MyHashMap<K, V>
 
     public void put(K key, V value){
 
-        int index = key.hashCode()%size;
-        Keys.add(index, key);
-        Values.add(index, value);
+        int index = abs(key.hashCode()%size);
+        for (int i=index;i<size;i++){
+            if (Keys.get(i)==null) {
+                Keys.add(i,key);
+                Values.add(i,value);
+                break;
+            } else if (Keys.get(i)==key) {
+                Keys.set(i,key);
+                Values.set(i,value);
+                break;
+            }
+        }
     }
 
-    public void remowe(K key){
+    public void remove(K key){
         if (Keys.contains(key)) {
             int index = Keys.indexOf(key);
             Keys.remove(index);
@@ -44,11 +55,5 @@ public class MyHashMap<K, V>
     Getting getting = (key) -> (Values.get( key.hashCode()%size));
 
     IsIn isin = (key) -> (Keys.contains(key));
-
-
-
-
-
-
 
 }
